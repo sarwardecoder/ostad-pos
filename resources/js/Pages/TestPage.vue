683ad002d74331b5830b4aafdcfb30c7d4f18cc3
Sarwar Jahan <sarwar3@gmail.com>
2025-04-04 04:12:41 +0600

    Ostad POS Project

---
+++ resources/js/Pages/TestPage.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+
+<template>
+    <h1 class="text-warning">This is test Page</h1>
+    <Link href="/">Home Page</Link>
+</template>
+
+<style scoped>
+</style>
